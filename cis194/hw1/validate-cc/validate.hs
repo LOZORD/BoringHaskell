18dd759561beb2a453cb38fa46a1ddbcd3166942
LOZORD <ljrudberg@gmail.com>
2016-06-02 13:53:42 -0500

    Finished first part of HW1

--- cis194/hw1/validate-cc/validate.hs
+++ cis194/hw1/validate-cc/validate.hs
@@ -23,3 +23,24 @@ doubleEveryOther list = reverse (doubleEveryOtherLeftToRight (reverse list))
 doubleEveryOtherLeftToRight list = multiplyPairs (list) (cycle [1, 2])
   -- zipWith takes 2 lists and creates a new list of paired elements
   where multiplyPairs = zipWith (*)
+
+-- Exercise 3
+sumDigits :: [Integer] -> Integer
+sumDigits list =
+  let
+      -- first create a list of lists of digits
+      -- it will look like [[1,6],[7],[1,2],[5]] using the example
+      digitsList = map (toDigits) list
+      -- then sum up all of the sublists
+      -- using the same example, we should get [7, 7, 3, 5]
+      digitSums  = map (sum) digitsList
+  in sum digitSums -- then, all we need to do is just sum the main list!
+
+-- Exercise 4
+validate :: Integer -> Bool
+validate creditCardNumber =
+  let digits = toDigits creditCardNumber
+      doubledEveryOtherDigits = doubleEveryOther digits
+      sumResult = sumDigits doubledEveryOtherDigits
+      modRemainder = sumResult `mod` 10
+  in modRemainder == 0
