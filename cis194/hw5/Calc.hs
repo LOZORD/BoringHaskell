d5b030f62d298a12ca24a9758c026bc0fe9ceadc
LOZORD <ljrudberg@gmail.com>
2016-06-29 21:41:13 -0500

    Completed Ex1 and Ex2 in HW5

--- cis194/hw5/Calc.hs
+++ cis194/hw5/Calc.hs
@@ -1,3 +1,24 @@
 module Calc where
 
 import ExprT
+import Parser (parseExp)
+
+-- Exercise 1
+
+eval :: ExprT -> Integer
+-- base case
+eval (Lit n) = n
+-- recursive cases
+eval (Add e1 e2) = (eval e1) + (eval e2)
+eval (Mul e1 e2) = (eval e1) * (eval e2)
+
+-- Exercise 2
+
+evalStr :: String -> Maybe Integer
+evalStr input =
+  let parse = parseExp (Lit) (Add) (Mul) input
+  in case parse of
+    Nothing -> Nothing
+    Just expr -> Just (eval expr)
+
+-- Exercise 3 -- TODO
