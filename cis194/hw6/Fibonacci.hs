63cf0b01f3455b7f985b2dea7a31a1cd3fb9875e
LOZORD <ljrudberg@gmail.com>
2016-07-12 18:45:38 -0500

    Completed HW6.2

--- cis194/hw6/Fibonacci.hs
+++ cis194/hw6/Fibonacci.hs
@@ -1,5 +1,7 @@
 module Fibonacci where
 
+import Data.List (unfoldr)
+
 -- Exercise 1
 
 fib :: Integer -> Integer
@@ -9,3 +11,18 @@ fib n = (fib (n - 1)) + (fib (n - 2))
 
 fibs1 :: [Integer]
 fibs1 = map (fib) [0..]
+
+-- Exercise 2
+
+-- A helper function for fibs2.
+-- Given a pair (a, b), f2' calculates c.
+-- From here, unfoldr appends a and uses (b, c) as the value pair for the next
+-- iteration of unfoldr on the list.
+f2' :: Num n => (n, n) -> Maybe (n, (n, n))
+f2' (a, b) =
+  let c = a + b
+  in Just (a, (b, c))
+
+-- Create a list via unfoldr using the initial pair (0, 1).
+fibs2 :: [Integer]
+fibs2 = unfoldr (f2') (0, 1)
